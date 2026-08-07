58fbf695421628f76ec2c40e087fffe1c1acc54f
Nikhilvicky01 <112875164+Nikhilvicky01@users.noreply.github.com>
2026-08-08 00:01:45 +0530

    31 - Next Permutation

---
+++ 0031-next-permutation/0031-next-permutation.java
@@ -0,0 +1,41 @@
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int n = nums.length;
+        int index = -1;
+        for(int i = n-2;i>=0;i--){
+            if(nums[i] < nums[i+1]){
+                index = i;
+                break;
+            }
+        }
+        if(index == -1){
+            reverse(nums,0,n-1);
+        }else{
+        for(int j = n-1;j > index;j--){
+            if(nums[j] > nums[index]){
+                int temp = nums[j];
+                nums[j] = nums[index];
+                nums[index] = temp;
+                break;
+            }
+        }
+        reverse(nums, index + 1, n - 1);
+        }
+        
+
+    }
+    public void reverse(int nums[],int left,int right){
+        while(left < right ){
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+            left ++ ;
+            right -- ;
+        }
+    }
+
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
