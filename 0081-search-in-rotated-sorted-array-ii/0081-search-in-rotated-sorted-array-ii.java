a2f3c4d4e524f52c7d368e45c4e39ca6106ea469
Nikhilvicky01 <112875164+Nikhilvicky01@users.noreply.github.com>
2026-08-14 01:30:51 +0530

    optimal approach

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -1,10 +1,33 @@
 class Solution {
     public boolean search(int[] nums, int target) {
-        for(int i = 0;i < nums.length;i ++){
-            if(nums[i] == target){
+        int l = 0;
+        int h = nums.length - 1;
+
+        while(l <= h ){
+            int mid = (l + h)/2;
+
+            if(nums[mid] == target){
                 return true;
             }
-        }
+
+            //nhi pata ki konsa side sorted hai 
+            if(nums[l] == nums[mid] && nums[h] == nums[mid]){
+                l ++ ;
+                h -- ;
+            }else if( nums[l] <= nums[mid]){// left sorted array
+                if(nums[l] <= target && nums[mid] > target){
+                    h = mid - 1;
+                }else{
+                    l = mid + 1;
+                }
+            }else{
+                if(nums[mid] < target && nums[h] >= target){
+                    l = mid + 1;
+                }else{
+                    h = mid - 1;
+                }
+            }
+        }    
         return false;
     }
 }
